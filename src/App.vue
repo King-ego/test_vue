ac96cb46148ccfd142076ff3a84a6f700e4efe00
Diego Santos <diegosantos@gmail.com>
2022-09-19 23:54:52 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>ola</div>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
-@Options({
-  components: {
-    HelloWorld,
+export default defineComponent({
+  components: {},
+  data() {
+    return {}
   },
 })
-export default class App extends Vue {}
 </script>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
