3f981ff4aa4ec825cbd527dd84db83b14cae9ff1
Diego Santos <diegosantos@gmail.com>
2022-09-22 19:08:48 -0300

    k

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push --force git@github.com:King-ego/test_vue.git  main:gh-pages
+git push --force git@github.com:King-ego/test_vue.git  main:gh-page
 
 cd -
